3151bfaee2cd6414202e693b4707123bb6f01009
ruiling <603412564@qq.com>
2020-03-31 21:16:13 +0800

    springboot

---
+++ src/main/java/com/ruiling/SpringBoot06DataMybatisApplication.java
@@ -0,0 +1,15 @@
+package com.ruiling;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan(value = "com.ruiling.mapper")
+@SpringBootApplication
+public class SpringBoot06DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06DataMybatisApplication.class, args);
+    }
+
+}
